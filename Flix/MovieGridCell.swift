df9525da8363d22449880c256715e48b2b0dc3f4
alexmeigz <alexmei@ucsb.edu>
2020-10-28 19:31:20 -0700

    part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Alex Mei on 10/28/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
